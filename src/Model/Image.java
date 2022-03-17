c08a31d96d5df286d79b6018122c6355e7d6ebe5
Thiago <60614957+thiagoferreira23@users.noreply.github.com>
2022-03-17 10:25:03 -0400

    Initial commit

---
+++ src/Model/Image.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * <p>Description de la classe</p>
+ *
+ * @author Franco Huynh, Gabriel M. Borges & Thiago Ferreira
+ * @version 1.0
+ * @since 2022-03-17 9:56 a.m.
+ */
+public class Image {
+}
