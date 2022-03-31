ef9889360c46dc81a9e7de9ee529f9ccf2c326a2
Thiago <60614957+thiagoferreira23@users.noreply.github.com>
2022-03-31 13:32:01 -0400

    Ajout de l'implémentation ActionListener

--- src/Controller/MenuOptionsControleur.java
+++ src/Controller/MenuOptionsControleur.java
@@ -1,4 +1,26 @@
 package Controller;
 
-public class MenuOptionsControleur {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * <p>
+ * TODO : Description de classe
+ * </p>
+ *
+ * @author Thiago Ferreira
+ * @version 1.0
+ * @since 2022-03-28 8:33 p.m.
+ */
+public class MenuOptionsControleur implements ActionListener {
+
+    public MenuOptionsControleur(){
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 }
