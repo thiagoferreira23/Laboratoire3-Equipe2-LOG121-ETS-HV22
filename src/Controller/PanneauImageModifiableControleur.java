6cbb9cba36beed535c0f1b97f403922cdff65a35
Gabriel Marrero <cesarmarrero7@gmail.com>
2022-04-06 20:12:54 -0400

    Gabriel: Ajout des methode dans le controlleur pour la commande zoom

--- src/Controller/PanneauImageModifiableControleur.java
+++ src/Controller/PanneauImageModifiableControleur.java
@@ -2,9 +2,10 @@ package Controller;
 
 import Commande.ZoomCommande;
 import Model.PerspectiveImageModifiable;
+import View.VuePanneauImage;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * <p>Description de la classe</p>
@@ -13,18 +14,56 @@ import java.awt.event.ActionListener;
  * @version 1.0
  * @since 2022-03-17 9:57 a.m.
  */
-public class PanneauImageModifiableControleur implements ActionListener {
-
-    private PerspectiveImageModifiable perspectiveImageModifiable;
-
-    public void zoomIn(){
-        ZoomCommande zoomCommande = new ZoomCommande(perspectiveImageModifiable,2);
-
-        zoomCommande.execute();
+public class PanneauImageModifiableControleur implements MouseListener {
+    private VuePanneauImage vueImage;
+    private PerspectiveImageModifiable perspective;
+    public void PanneauImageAvecChangementsControleur(VuePanneauImage vuePanneau, PerspectiveImageModifiable perspective) {
+        this.vueImage = vuePanneau;
+        this.perspective = perspective;
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void mouseClicked(MouseEvent e) {
+        Point imagePosition = vueImage.getGraphics().getClipBounds().getLocation(); //GET IMAGE POSITION
+        Point mousePosition = e.getPoint();
+        int nvPositionX = (int) (imagePosition.getX() + mousePosition.getX());
+        int nvPositionY = (int) (imagePosition.getY() + mousePosition.getY());
+        //this.vueImage.getGraphics().set()... //SET NEW POSITION
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
 
     }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void mouseWheelMoved(MouseWheelEvent e) {
+
+        int notches = e.getWheelRotation();
+        double temp = perspective.getZoom() - (notches * 0.2);
+
+        temp = Math.max(temp, 1.0);
+        if (temp != perspective.getZoom()) {
+            perspective.setZoom(temp);
+            //resizeImage();
+        }
+        // double factor = Math.pow(perspective.getZoom(), 0);
+        //perspective.setSize((int) (vueImage.getWidth() * factor), (int) (vueImage.getHeight() * factor));
+
+    }
+
 }
